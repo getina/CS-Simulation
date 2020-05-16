271bf68bfe19a85cdca272b181a2765810f386d5
kevinlin29 <61879285+kevinlin29@users.noreply.github.com>
2020-05-15 22:59:35 -0400

    kl

--- Fireball.java
+++ Fireball.java
@@ -2,25 +2,31 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Fireball here.
- * 
+ *
  * @author Kevin Lin
  * @version 2020-0421
  */
 public class Fireball extends Projectile
 {
     /**
-     * 
+     *
      */
     private int imgNum = 1;
     private boolean animationComplete = false;
     GreenfootImage [] imgs = new GreenfootImage[38];
-    public void act() 
+    public void act()
     {
         update();
-    }    
+    }
     //negative diretion to launch left, positve to launch right. Any integer value
     //power, angle, explosion height and gravity can be changed
-    
+    /**
+     * @param power The power which the fireball is launched
+     * @param theta The angle which the fireball is launched
+     * @param dir The direction which the fireball is launched
+     * @param exploHeight The height at which the fireball explosdes
+     * @param grav The gravitation constant
+     */
     public Fireball(int power, int theta, int dir, int exploHeight, int grav){
         gravity = grav;
         speed = power;
@@ -30,7 +36,12 @@ public class Fireball extends Projectile
         defineV(speed,angle);
         setImg();
     }
-    
+    /**
+     * @param power The power which the fireball is launched
+     * @param theta The angle which the fireball is launched
+     * @param dir The direction which the fireball is launched
+     * @param exploHeight The height at which the fireball explosdes
+     */
     public Fireball(int power, int theta, int dir, int exploHeight){
         gravity = 10;
         speed = power;
@@ -40,7 +51,11 @@ public class Fireball extends Projectile
         defineV(speed,angle);
         setImg();
     }
-    
+    /**
+     * @param power The power which the fireball is launched
+     * @param theta The angle which the fireball is launched
+     * @param dir The direction which the fireball is launched
+     */
     public Fireball(int power, int theta, int dir){
         gravity = 10;
         speed = power;
@@ -50,7 +65,9 @@ public class Fireball extends Projectile
         defineV(speed,angle);
         setImg();
     }
-    
+    /**
+     * @param dir The direction which the fireball is launched
+     */
     public Fireball(int dir){
         speed = 100;
         angle = 20;
@@ -59,20 +76,22 @@ public class Fireball extends Projectile
         setImg();
     }
     //defining the x and y axis velocity
-    
+
     private void defineV(int speed, int angle){
         vX = (double)speed * Math.cos((double)angle*Math.PI/180.0) ;
         vY = -(double)speed * Math.sin((double)angle*Math.PI/180.0);
     }
-    
-    //update method
+
+    /**
+     * This method updates the location of the fireball, and detects where to explode
+     */
     public void update(){
         if(direction >= 0){
             setLocation(getX() + (int)vX, getY() + (int)vY);
         }else{
             setLocation(getX() - (int)vX, getY() + (int)vY);
         }
-        
+
         if(getY() > getWorld().getHeight() - height){
             vX = 0;
             vY = 0;
@@ -82,22 +101,22 @@ public class Fireball extends Projectile
         }
         checkGround();
     }
-    
+
     //remove object once animation is complete
     public void checkGround(){
         if(animationComplete){
             remove();
         }
     }
-    
+
     //loading images into array
     private void setImg(){
         for(int i = 2; i < 40; i++){
             imgs[i-2] = new GreenfootImage( "exp " + i + ".png");
         }
         setImage( imgs[imgNum] );
-    }  
-    
+    }
+
     //cycle through images for animation, and set animation completion to true
     private void animate(){
         imgNum = ( imgNum + 1 ) % imgs.length;
